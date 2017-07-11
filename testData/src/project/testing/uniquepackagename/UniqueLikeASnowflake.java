d10bf855ec9eedf6ad03b9710325cccdb502005a
Anya Yaveyn <anya.yaveyn@yandex.ru>
2017-07-11 20:52:49 +0300

    test data added

---
+++ testData/src/project/testing/uniquepackagename/UniqueLikeASnowflake.java
@@ -0,0 +1,11 @@
+package project.testing.uniquepackagename;
+
+/**
+ * @author bronti.
+ */
+public class UniqueLikeASnowflake {
+    private void privateMethod666() {}
+    public void publicMethodDanceWithMe() {}
+    private int privateFieldOhSoPrivate;
+    public int publicFieldWantsToSleep;
+}
